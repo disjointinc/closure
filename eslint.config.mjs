1676b0f66219207d946d0280f0619d57f98949f1
Colin Parsons <c.parsons.429@gmail.com>
2026-08-19 15:12:20 -0400

    deps(): use tanstack start for web

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 import tseslint from "typescript-eslint";
 
 export default [
+  {
+    ignores: ["web/dist/", "web/src/routeTree.gen.ts"],
+  },
   ...tseslint.configs.recommended,
   {
     files: ["**/*.{js,mjs,ts,tsx}"],
