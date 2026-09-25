516679086426818ce8a03324493f0e01bdc12c79
Colin Parsons <c.parsons.429@gmail.com>
2026-09-25 15:20:16 -0400

    fix(garbage-collection): remove test resource loop from production

--- api/server.ts
+++ api/server.ts
@@ -14,7 +14,6 @@ import {
 import { startMeteringReconcileLoop } from "./cache/meter/reconcile.ts";
 import { startRuleExecutorLoop } from "./cache/rule/execute.ts";
 import { startRuleSchedulerLoop } from "./cache/rule/schedule.ts";
-import { startTestSuiteResourceCollectionLoop } from "./garbage-collection/test-suite-resources.ts";
 import app from "./handler.ts";
 
 await rebuildMissingMeterBalances();
@@ -22,9 +21,6 @@ startMeteringFlushLoop();
 startMeteringReconcileLoop();
 startRuleSchedulerLoop();
 startRuleExecutorLoop();
-if (config.garbageCollection.testSuiteResources.enabled) {
-  startTestSuiteResourceCollectionLoop();
-}
 
 serve(
   {
