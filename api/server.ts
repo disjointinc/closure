2bfa0507627217f5a20b3004a9bf5a6489813857
Colin Parsons <c.parsons.429@gmail.com>
2026-09-25 15:40:56 -0400

    feat(garbage-collection): standardize call sites

--- api/server.ts
+++ api/server.ts
@@ -14,6 +14,7 @@ import {
 import { startMeteringReconcileLoop } from "./cache/meter/reconcile.ts";
 import { startRuleExecutorLoop } from "./cache/rule/execute.ts";
 import { startRuleSchedulerLoop } from "./cache/rule/schedule.ts";
+import { startGarbageCollectionLoop } from "./garbage-collection/loop.ts";
 import app from "./handler.ts";
 
 await rebuildMissingMeterBalances();
@@ -21,6 +22,7 @@ startMeteringFlushLoop();
 startMeteringReconcileLoop();
 startRuleSchedulerLoop();
 startRuleExecutorLoop();
+startGarbageCollectionLoop();
 
 serve(
   {
